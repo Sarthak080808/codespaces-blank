25b39393ccfcf855c6bcf13059a7a9c142af4eb9
Sarthak080808 <124574564+Sarthak080808@users.noreply.github.com>
2023-04-10 16:58:38 +0000

    first commit

---
+++ problem_1_2_b.py
@@ -0,0 +1,8 @@
+"""
+(b) How one can check if the restriction is on a number of words rather than letters? Write a function that allows a message with only 30 words
+"""
+
+a=input()
+word_list=a.split()[:30]
+output=" ".join(word_list)
+print(output)
