39f579b499ed4df4f2889aaa543dc38fa93f93dc
Sarthak080808 <124574564+Sarthak080808@users.noreply.github.com>
2023-04-11 05:52:29 +0000

    main

--- problem_2_c.py
+++ problem_2_c.py
@@ -7,4 +7,4 @@ w=float(input("Enter wind speed in Km/h: "))
 def P_F_T(T,w):
     output= 13.12 + 0.6215*T - 11.37*(w**0.16) + 0.3965*T*(w**0.16) 
     return round(output,2)
-print(P_F_T(T,w),"°C")
+print(P_F_T(T,w),"°C"
